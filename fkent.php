316f098c6e93b5854e5b1abc399656530af78002
fredkentofficial <golobafred67@gmail.com>
2026-04-09 12:49:44 -0700

    Add files via upload

---
+++ fkent.php
@@ -0,0 +1,6 @@
+<?php
+    echo "Hello, World!";
+    echo "Welcome to PHP programming.";
+    echo "am fred kent and i am a software developer.";
+    echo "i have experience in web development and software engineering.";
+?>
---
+++ variables.php
@@ -0,0 +1,9 @@
+<?php
+ $name = "Fred Kent";
+ $age;
+ $price = 19.99;
+ $isStudent = true;
+ 
+ echo gettype($name) . "<br>";
+ 
+?>
